1f7bc638eec072477adb2c20f65208f1355ed318
Keyhan <callofassasin@yahoo.com>
2016-08-22 19:59:01 +0430

    Initial Commit

---
+++ pokedexApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedexApp
+//
+//  Created by Keyhan on 8/22/16.
+//  Copyright © 2016 Keyhan Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
