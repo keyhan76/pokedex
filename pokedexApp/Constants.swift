27436b8672b66ad386694e3d7f4b6d660dc15b0a
Keyhan <Keyhan@Keyhans-MacBook-Pro.local>
2016-08-31 16:03:31 +0430

    I just finished my pokedex app

---
+++ pokedexApp/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedexApp
+//
+//  Created by Keyhan on 8/29/16.
+//  Copyright © 2016 Keyhan Inc. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
---
+++ pokedexApp/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  pokedexApp
+//
+//  Created by Keyhan on 8/23/16.
+//  Copyright © 2016 Keyhan Inc. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
